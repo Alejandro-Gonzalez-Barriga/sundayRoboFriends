3dd9e1472a0bbd9d30deaf2eca26b20202131b58
AlexG <alejandro.gonzalez.barriga@gmail.com>
2019-03-10 18:42:19 -0600

    defactoring with .map!

--- chilangabanda/src/index.js
+++ chilangabanda/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';//this is the painter that renders
-import ReactDOM from 'react-dom';//used for the website, its like the glue to the dom
-import './index.css';//imports styling to the components through App.js
-import App from './App';//
-import Hello from './Hello'
-import 'tachyons'
+import React from 'react';//this is the painter that renders the components, imports files and pkgs from react,robot that does dom manipulation
+import ReactDOM from 'react-dom';//dom website , onto canvas of website
+import './index.css';//add css to each different componment to aplly to app.js component the ./ is in same folder
+//import App from './App';//if no dot it assumes its javascript
+//import MyComponent from "./MyComponent.js"
 import * as serviceWorker from './serviceWorker';
-//sets the canvas for the js to the place where the id is
-ReactDOM.render(<Hello greeting={'Hello' + 'React ninja'}/>, document.getElementById('root'));
+
+import CardList from "./CardList";
+//library for css dircetly applying to jsx , html
+import "tachyons";
+import {robots} from "./Robots";
+ReactDOM.render(
+<CardList robots={robots} />
+, document.getElementById('root'));//what react does , use function render , want to render app to document (root)
+// render funstion takes 2 arguments what you are rendering from to where you are rendering
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
